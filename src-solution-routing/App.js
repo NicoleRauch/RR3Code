e1532aa1a1dbf72a96b82de6a380570b18f0884c
Nicole Rauch <info@nicole-rauch.de>
2017-11-28 20:09:12 +0100

    Fixing a new Router constraint. (A warning is left unfixed, to be done later.)

--- src-solution-routing/App.js
+++ src-solution-routing/App.js
@@ -1,9 +1,5 @@
 import React, {Component} from "react";
-import {
-  HashRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import {HashRouter as Router, Link, Route} from 'react-router-dom';
 
 import HelloWorld from "../src-solution-react/HelloWorldFunctionalComponent";
 import User from "../src-solution-react/UserWithHighlight";
@@ -11,9 +7,9 @@ import UserList from "../src-solution-react/UserListWithHighlight";
 import UserCreationViaInternalState from "../src-solution-react/UserCreationViaInternalState";
 
 const users = [
-    {firstName: "Nicole", lastName: "Rauch"},
-    {firstName: "Peter", lastName: "Müller"},
-    {firstName: "Fritz", lastName: "Walter"}
+  {firstName: "Nicole", lastName: "Rauch"},
+  {firstName: "Peter", lastName: "Müller"},
+  {firstName: "Fritz", lastName: "Walter"}
 ];
 
 export default class extends Component {
@@ -21,17 +17,23 @@ export default class extends Component {
   render() {
 
     return (
-    <Router>   <ul>
-        <li><Link to="/">Hello World</Link></li>
-        <li><Link to="/user/Nicole/Rauch">User</Link></li>
-        <li><Link to="/users">User List</Link></li>
-        <li><Link to="/creation">User Creation</Link></li>
-      </ul>   <hr/>
-      <Route exact path="/" component={HelloWorld}/>
-      <Route path="/user/:firstName/:lastName" render={({match}) => <User firstName={match.params.firstName} lastName={match.params.lastName}/>} />
-      <Route path="/users" render={() => <UserList users={users} />} />
-      <Route path="/creation" render={() => <UserCreationViaInternalState submitUser={ () => {} } />} />
-    </Router>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Hello World</Link></li>
+            <li><Link to="/user/Nicole/Rauch">User</Link></li>
+            <li><Link to="/users">User List</Link></li>
+            <li><Link to="/creation">User Creation</Link></li>
+          </ul>
+          <hr/>
+          <Route exact path="/" component={HelloWorld}/>
+          <Route path="/user/:firstName/:lastName"
+                 render={({match}) => <User firstName={match.params.firstName} lastName={match.params.lastName}/>}/>
+          <Route path="/users" render={() => <UserList users={users}/>}/>
+          <Route path="/creation" render={() => <UserCreationViaInternalState submitUser={() => {
+          }}/>}/>
+        </div>
+      </Router>
     );
   }
 }
