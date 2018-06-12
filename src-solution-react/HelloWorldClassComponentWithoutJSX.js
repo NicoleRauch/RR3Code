220762bad3665551f4ee67daab1281f3e6a6d669
Nicole Rauch <info@nicole-rauch.de>
2018-06-12 14:20:45 +0200

    Now the components have names (for React DevTools)

--- src-solution-react/HelloWorldClassComponentWithoutJSX.js
+++ src-solution-react/HelloWorldClassComponentWithoutJSX.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class extends Component {
+export default class HelloWorld extends Component {
 
   render() {
     return React.createElement('p', null, 'Hello World!');
--- src-solution-react/HelloWorldFunctionalComponent.js
+++ src-solution-react/HelloWorldFunctionalComponent.js
@@ -1,3 +1,5 @@
 import React from "react";
 
-export default () => <p>Hello World!</p>;
+const HelloWorld = () => <p>Hello World!</p>;
+
+export default HelloWorld;
